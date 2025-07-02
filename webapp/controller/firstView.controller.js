f1e958f0b9a0674eada26eb4741ae203c684c7fb
Ema <emanuele.bellotti@sileron.com>
2025-07-02 10:16:30 +0200

    V3

--- webapp/controller/firstView.controller.js
+++ webapp/controller/firstView.controller.js
@@ -4,11 +4,26 @@ sap.ui.define(
     "use strict";
 
     return Controller.extend("rfinterface.controller.firstView", {
-      onInit: function () {},
+      onInit: function () {
+        // Recupero la stringa con la chiave
+        var sText = this.getOwnerComponent()
+          .getModel("i18n")
+          .getResourceBundle()
+          .getText("Accept");
+
+        // Prendo il bottone per id locale e cambio il testo
+        this.byId("_IDGenButton1").setText(sText);
+      },
 
       onScanSuccessOne: function (oEvent) {
         if (oEvent.getParameter("cancelled")) {
-          MessageToast.show("Scan cancelled", { duration: 1000 });
+          // Recupero la stringa con la chiave
+          var sText = this.getOwnerComponent()
+            .getModel("i18n")
+            .getResourceBundle()
+            .getText("ScanCancelled");
+
+          MessageToast.show(sText, { duration: 1000 });
         } else {
           if (
             oEvent.getParameter("text") !== undefined &&
@@ -19,7 +34,11 @@ sap.ui.define(
               oInput.setValue(oEvent.mParameters.text);
             }
           } else {
-            MessageToast.show("No QR code detected", { duration: 1000 });
+            var sText = this.getOwnerComponent()
+              .getModel("i18n")
+              .getResourceBundle()
+              .getText("NoBarcodeDetected");
+            MessageToast.show(sText, { duration: 1000 });
 
             var oInput = this.byId("sampleBarcodeScannerResultTwo");
             if (oInput) {
@@ -33,15 +52,7 @@ sap.ui.define(
         const oView = this.getView();
         const oModel = this.getView().getModel(); // modello OData
         const sBarcode = oView.byId("sampleBarcodeScannerResultTwo").getValue();
-
-        var oEmptyModel = new sap.ui.model.json.JSONModel({
-          Batch: "",
-          Plant: "",
-          Product: "",
-          Productdescription: "",
-          Productstandardid: "",
-        });
-        oView.setModel(oEmptyModel, "model");
+        var that = this; // salva il riferimento al controller
 
         if (sBarcode) {
           oModel.callFunction("/ConvertToean", {
@@ -51,32 +62,77 @@ sap.ui.define(
               barcode: sBarcode,
             },
             success: function (oData) {
-              var oJSONModelHeader = new sap.ui.model.json.JSONModel({
-                Batch: oData.ConvertToean.Batch,
-                Plant: oData.ConvertToean.Plant,
-                Product: oData.ConvertToean.Product,
-                Productdescription: oData.ConvertToean.Productdescription,
-                Productstandardid: oData.ConvertToean.Productstandardid,
-              });
-
-              oView.setModel(oJSONModelHeader, "model");
-
-              // Rendi visibile il blocco Result
-              //oView.byId("_IDGenGroupElement2").setVisible(true);
               if (oData.ConvertToean.Message) {
                 MessageBox.error(oData.ConvertToean.Message);
               } else {
+                var oEmptyModel = new sap.ui.model.json.JSONModel({
+                  Batch: "",
+                  Plant: "",
+                  Product: "",
+                  Productdescription: "",
+                  Productstandardid: "",
+                });
+                oView.setModel(oEmptyModel, "model");
+
+                var oJSONModelHeader = new sap.ui.model.json.JSONModel({
+                  Barcode: sBarcode,
+                  EAN: oData.ConvertToean.Productstandardid,
+                  Batch: oData.ConvertToean.Batch,
+                  Product: oData.ConvertToean.Product,
+                  Productdescription: oData.ConvertToean.Productdescription,
+                });
+
+                oView.setModel(oJSONModelHeader, "model");
+
+                // Rendi visibile il blocco Result
+                //oView.byId("_IDGenGroupElement2").setVisible(true);
+                var sText_1 = that
+                  .getOwnerComponent()
+                  .getModel("i18n")
+                  .getResourceBundle()
+                  .getText("DataLoad");
+                var sText_2 = that
+                  .getOwnerComponent()
+                  .getModel("i18n")
+                  .getResourceBundle()
+                  .getText("Batch");
+                var sText_3 = that
+                  .getOwnerComponent()
+                  .getModel("i18n")
+                  .getResourceBundle()
+                  .getText("Present");
+
                 MessageBox.success(
-                  "Data load from " + oData.ConvertToean.Product + " material."
+                  sText_1 +
+                    "\n" +
+                    sText_2 +
+                    " : " +
+                    oData.ConvertToean.Batch +
+                    " " +
+                    sText_3 +
+                    " : " +
+                    oData.ConvertToean.Product
                 );
               }
+
+              oView.byId("sampleBarcodeScannerResultTwo").setValue("");
             },
             error: function (oError) {
-              MessageBox.error("Error during data fetch");
+              var sText = that
+                .getOwnerComponent()
+                .getModel("i18n")
+                .getResourceBundle()
+                .getText("ErrorDuring");
+              MessageBox.error(sText);
             },
           });
         } else {
-          MessageBox.error("Please enter a barcode");
+          var sText = that
+            .getOwnerComponent()
+            .getModel("i18n")
+            .getResourceBundle()
+            .getText("PleaseEnter");
+          MessageBox.error(sText);
         }
       },
     });
